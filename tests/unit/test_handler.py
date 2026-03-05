ed6f98076c9a41302ab95f3e4777259a29a188bd
Dushyant Pal <duspal@amazon.com>
2026-03-05 05:43:18 +0000

    Breaking the lambda function on purpose

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -62,6 +62,10 @@ def apigw_event():
     }
 
 
+def test_lambda_handler(apigw_event, mocker):
+    assert True
+
+
 def test_lambda_handler(apigw_event):
 
     ret = app.lambda_handler(apigw_event, "")
